be764fca44a174c3bbb387cc608a8892f3542f46
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-23 13:17:17 +0900

    Update mortgage_type.dart

--- lib/components/mortgage_type.dart
+++ lib/components/mortgage_type.dart
@@ -26,9 +26,14 @@ class _MortgageTypeState extends State<MortgageType> {
         const SizedBox(height: 10),
         Container(
           decoration: BoxDecoration(
+            color: widget.mortgage == Mortgage.repayment
+                ? const Color.fromRGBO(249, 249, 219, 1)
+                : Colors.transparent,
             border: Border.all(
               width: 1,
-              color: const Color.fromRGBO(184, 192, 197, 1),
+              color: widget.mortgage == Mortgage.repayment
+                  ? const Color.fromRGBO(209, 218, 41, 1)
+                  : const Color.fromRGBO(184, 192, 197, 1),
             ),
             borderRadius: BorderRadius.circular(10),
           ),
@@ -42,7 +47,7 @@ class _MortgageTypeState extends State<MortgageType> {
               ),
             ),
             leading: Radio(
-              activeColor: const Color.fromRGBO(19, 42, 56, 1),
+              activeColor: const Color.fromRGBO(212, 214, 42, 1),
               value: Mortgage.repayment,
               groupValue: widget.mortgage,
               onChanged: (value) {
@@ -60,9 +65,14 @@ class _MortgageTypeState extends State<MortgageType> {
           decoration: BoxDecoration(
             border: Border.all(
               width: 1,
-              color: const Color.fromRGBO(184, 192, 197, 1),
+              color: widget.mortgage == Mortgage.interestOnly
+                  ? const Color.fromRGBO(209, 218, 41, 1)
+                  : const Color.fromRGBO(184, 192, 197, 1),
             ),
             borderRadius: BorderRadius.circular(10),
+            color: widget.mortgage == Mortgage.interestOnly
+                ? const Color.fromRGBO(249, 249, 219, 1)
+                : Colors.transparent,
           ),
           child: ListTile(
             title: const Text(
@@ -74,7 +84,7 @@ class _MortgageTypeState extends State<MortgageType> {
               ),
             ),
             leading: Radio(
-              activeColor: const Color.fromRGBO(19, 42, 56, 1),
+              activeColor: const Color.fromRGBO(212, 214, 42, 1),
               value: Mortgage.interestOnly,
               groupValue: widget.mortgage,
               onChanged: (value) {
